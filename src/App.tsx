b1ac5ebc3529d8b22d89e4025f40f5b9a6ed58a0
Shynggys Umbetov <shynggys@web-premiere.fr>
2021-07-05 00:10:40 +0200

    Fetch by portion button implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
 import './App.css';
@@ -6,13 +6,27 @@ import { useFetchBreedsQuery } from './features/dogs/dogs-api-slice'
 
 function App() {
 
-  const { data = [] } = useFetchBreedsQuery();
+  const [numDogs, setNumDogs] = useState(10);
+  const { data = [] } = useFetchBreedsQuery(numDogs);
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <Counter />
+        
+        <div>
+          <p>Dogs to fetch:</p>
+          <select 
+            value={numDogs} 
+            onChange={(e) => setNumDogs(Number(e.target.value))}
+            >
+            <option value="5">5</option>
+            <option value="10">10</option>
+            <option value="15">15</option>
+            <option value="20">20</option>
+          </select>
+        </div>
         
         <div>
           <p>Number of dogs fetched: {data.length}</p>
